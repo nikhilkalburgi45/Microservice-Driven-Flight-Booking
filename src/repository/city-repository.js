8ce537f5a82ce628c97e3feab3b9d528af53f070
nikhilkalburgi45 <nikhilkalburgi21@gmail.com>
2025-02-17 19:53:08 +0530

    🛠️ Added getCity and updateCity methods in CityRepository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -7,19 +7,48 @@ class CityRepository {
       const city = await City.create({ name });
       return city;
     } catch (error) {
+      console.log("Something went wrong on the repo layer");
       throw { error };
     }
   }
 
   //   2 delete a city based on id
-  async deleteCity(cityId)  {
+  async deleteCity(cityId) {
     try {
       await City.destroy({
         where: {
           id: cityId,
         },
       });
+      return true;
     } catch (error) {
+      console.log("Something went wrong on the repo layer");
+      throw { error };
+    }
+  }
+
+  //   3 fetch city
+  async getCity(cityId) {
+    try {
+      const city = await City.findByPk(cityId);
+      return city;
+    } catch (error) {
+      console.log("Something went wrong on the repo layer");
+      throw { error };
+    }
+  }
+  
+  //   4 update city
+  async updateCity(cityId, data) {
+    try {
+      const city = await City.update(data, {
+        where: {
+          id: cityId,
+        },
+      });
+      return city;
+    } catch (error) {
+      console.log("Something went wrong on the repo layer");
       throw { error };
     }
   }
