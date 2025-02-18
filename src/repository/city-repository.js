34834ad8e3cc38ad1cdfebe0773efbba08337632
nikhilkalburgi45 <nikhilkalburgi21@gmail.com>
2025-02-18 11:14:00 +0530

    minor fixes in update city

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -41,11 +41,14 @@ class CityRepository {
   //   4 update city
   async updateCity(cityId, data) {
     try {
-      const city = await City.update(data, {
-        where: {
-          id: cityId,
-        },
-      });
+      // const city = await City.update(data, {
+      //   where: {
+      //     id: cityId,
+      //   },
+      // });
+      const city = await City.findByPk(cityId);
+      city.name = data.name;
+      await city.save();
       return city;
     } catch (error) {
       console.log("Something went wrong on the repo layer");
