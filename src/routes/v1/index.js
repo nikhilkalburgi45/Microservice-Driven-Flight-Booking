14c69c324ef5c6d33ba6b271a340106da2c24a69
nikhilkalburgi45 <nikhilkalburgi21@gmail.com>
2025-02-18 10:51:35 +0530

    Added routes for featching,Updating,Delating a city

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -5,5 +5,11 @@ const router = express.Router();
 
 // POST :- /api/v1/city
 router.post("/city", CityController.create);
+// DELETE :- /api/v1/city/:id
+router.delete("/city/:id", CityController.destroy);
+// GET :- /api/v1/city/:id
+router.get("/city/:id", CityController.get);
+// PATCH :- /api/v1/city/:id
+router.patch("/city/:id", CityController.update);
 
 module.exports = router;
