612cc23658bc3479136d63b5e1b51942e9d7c1c2
nikhilkalburgi45 <nikhilkalburgi21@gmail.com>
2025-02-22 08:43:03 +0530

    Added common crud repository

---
+++ src/repository/crud-repositories.js
@@ -0,0 +1,59 @@
+class CrudRepository {
+  constructor(model) {
+    this.model = model;
+  }
+
+  async create(data) {
+    try {
+      const result = await this.model.create(data);
+      return result;
+    } catch (error) {
+      console.log("Something went on crud repo");
+    }
+  }
+
+  async destroy(modelId) {
+    try {
+      const result = await this.model.create({
+        where: {
+          id: modelId,
+        },
+      });
+      return true;
+    } catch (error) {
+      console.log("Something went on crud repo");
+    }
+  }
+
+  async get(modelId) {
+    try {
+      const result = await this.model.findByPk(modelId);
+      return result;
+    } catch (error) {
+      console.log("Something went on crud repo");
+    }
+  }
+
+  async getAll() {
+    try {
+      const result = await this.model.findAll();
+      return result;
+    } catch (error) {
+      console.log("Something went on crud repo");
+    }
+  }
+
+  async update(modelId, data) {
+    try {
+      const result = await this.model.update(data, {
+        where: {
+          id: modelId,
+        },
+      });
+    } catch (error) {
+      console.log("Something went on crud repo");
+    }
+  }
+}
+
+module.exports = CrudRepository;
